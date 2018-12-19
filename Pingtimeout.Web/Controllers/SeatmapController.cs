8b55b55cd4ed835802409ad4c0f90bad562d3efb
Thomas Kalve <thomas@tkalve.no>
2018-12-20 00:40:10 +0100

    Added seat map PDF print

--- Pingtimeout.Web/Controllers/SeatmapController.cs
+++ Pingtimeout.Web/Controllers/SeatmapController.cs
@@ -1,10 +1,16 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using PdfSharp.Drawing;
+using PdfSharp.Drawing.Layout;
+using PdfSharp.Pdf;
 using PingTimeout.Web.Data;
 using PingTimeout.Web.Models;
 
@@ -76,6 +82,52 @@ namespace PingTimeout.Web.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Print(int eventId)
+        {
+            var tmpFile = Path.GetTempFileName();
+
+            var activeEvent = eventId > 0 ? _context.Events.Find(eventId) : _context.Events.FirstOrDefault();
+
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = $"Seatmap {activeEvent.Name}";
+            var seats = _context.Seats.OrderBy(m => m.RowNumber).ThenBy(m => m.SeatNumber);
+            foreach (var seat in seats)
+            {
+                PdfPage page = document.AddPage();
+                page.Size = PdfSharp.PageSize.A4;
+                page.Orientation = PdfSharp.PageOrientation.Landscape;
+
+                XGraphics gfx = XGraphics.FromPdfPage(page);
+                XFont boldfont = new XFont("Courier New", 60, XFontStyle.Bold);
+                XFont font = new XFont("Courier New", 48, XFontStyle.Regular);
+                gfx.DrawString($"Rad {seat.RowNumber} | Sete {seat.SeatNumber}", boldfont, XBrushes.Black, new XRect(0, 0, page.Width, page.Height / 2), XStringFormats.BottomCenter);
+
+
+                var bottomRect = new XRect(50, page.Height / 2, page.Width-100, page.Height / 2);
+
+                string text = "(ikke reservert)";
+                var ticket = _context.Tickets.FirstOrDefault(t => t.Seat.Id == seat.Id);
+                if (ticket != null)
+                    text = $"{ticket.UserName}";
+                else if (seat.Disabled)
+                    text = $"Reservert for CREW";
+
+                gfx.DrawString(text, font, XBrushes.Gray, bottomRect, XStringFormats.TopCenter);
+
+            }
+
+            document.Save(tmpFile);
+
+            var stream = new FileStream(tmpFile, FileMode.Open);
+
+            if (stream == null)
+                return NotFound();
+
+            var date = DateTime.Now.ToShortDateString();
+            return File(stream, "application/pdf", $"{activeEvent.Name} seatmap {DateTime.Now.ToString("yyyy-MM-dd HHmmss")}");
         }
 
         public IActionResult Auth(string token)
